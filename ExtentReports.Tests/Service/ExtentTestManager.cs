87a27badeb16e0e1b48b2f4d3038859c625aff94
anshooarora <anshooarora@gmail.com>
2019-01-09 12:22:50 -0500

    remove unused imports

--- ExtentReports.Tests/Service/ExtentTestManager.cs
+++ ExtentReports.Tests/Service/ExtentTestManager.cs
@@ -1,12 +1,5 @@
-﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace AventStack.ExtentReports.Tests.Parallel
 {

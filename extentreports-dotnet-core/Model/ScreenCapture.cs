0243fde97ab15fbe03db418be4d3794b34223d62
anshooarora <anshooarora@gmail.com>
2020-02-01 19:51:15 -0800

    fixes #39

--- extentreports-dotnet-core/Model/ScreenCapture.cs
+++ extentreports-dotnet-core/Model/ScreenCapture.cs
@@ -26,10 +26,10 @@ namespace AventStack.ExtentReports.Model
             {
                 if (!string.IsNullOrEmpty(Base64String))
                 {
-                    return "<a href='" + Base64StringDataType + Base64String + "' data-featherlight='image'><span class='label grey badge white-text text-white'>base64-img</span></a>";
+                    return "<a href='" + Base64StringDataType + Base64String + "' data-featherlight='image' title='" + Title + "'><span class='label grey badge white-text text-white'>base64-img</span></a>";
                 }
 
-                return "<a class='r-img' onerror='this.style.display=\"none\"' data-featherlight='" + Path + "' href='" + Path + "' data-src='" + Path + "'>" +
+                return "<a class='r-img' onerror='this.style.display=\"none\"' data-featherlight='" + Path + "' href='" + Path + "' data-src='" + Path + "' title='" + Title + "'>" +
                         "<span class='label grey badge white-text text-white'>img</span>" +
                     "</a>";
             }
